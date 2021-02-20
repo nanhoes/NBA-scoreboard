d48c8a490dcc576c7ed1d7aedbb44c1b070d4b5d
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-02-19 18:29:06 -0800

    Update NBA_File_Handler.py

--- NBA_File_Handler.py
+++ NBA_File_Handler.py
@@ -7,7 +7,7 @@ class NBA_FILE_HANDLER:
     
     
     def __init__(self):
-        self.path = '/home/pi/NBA-scoreboard/'
+        self.path = '/home/pi/My-NBA-scoreboard/'
         
     def JSON_GAMEDATA(self, gamedata, date):
         
