57dde10ef197103b88fd1b4703ea013d8c99d351
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-02-28 16:29:04 -0800

    Update NBA_Render.py

--- NBA_Render.py
+++ NBA_Render.py
@@ -159,6 +159,48 @@ class Render:
                 awaystatline = awayleadername[0] + '.' + str(awayleaderlastname) + ' ' + str(awayleaderpoints) + '-' + str(awayleaderrebounds) + '-' + str(awayleaderassists)
 #                graphics.DrawText(canvas, self.font2, 1, 31, graphics.Color(self.team_colors[hometeam][0][0], self.team_colors[hometeam][0][1], self.team_colors[hometeam][0][2]), $
      
+                pos = 0
+                timeout = time.time() + 2
+                while True:
+                    for line in range(20,32):
+                        graphics.DrawLine(canvas, 0, line, 63, line, graphics.Color(0, 0, 0))
+                    len1 = graphics.DrawText(canvas, self.font2, pos, 31, graphics.Color(self.team_colors[awayteam][0][0], self.team_colors[awayteam][0][1], self.team_colors[awayteam][0][1]), awaystatline)
+  #                  len2 = graphics.DrawText(canvas, self.font2, pos, 31, graphics.Color(self.team_colors[hometeam][0][0], self.team_colors[hometeam][0][1], self.team_colors[hometeam][0][2]), homestatline)
+                    #if time.time() > timeout:
+                    if time.time() > timeout:
+                        pos -= 1
+                        if (pos + len1 < 0):
+                            pos = canvas.width
+#                    if (pos + len2 < 0):
+ #                       pos = canvas.width
+
+                    #if time.time() > timeout:
+                        if pos == -(len1-63) or len1 <= 63:                 
+                            time.sleep(2)
+                            break
+                    for line in range(20,32):
+                        graphics.DrawLine(canvas, 0, line, 63, line, graphics.Color(0, 0, 0))
+                    time.sleep(0.01)
+                    canvas = matrix.SwapOnVSync(canvas)
+
+
+
+                pos = 0
+                timeout = time.time() + 2
+                while True:         
+                    for line in range(20,32):
+                        graphics.DrawLine(canvas, 0, line, 63, line, graphics.Color(0, 0, 0))
+                    len2 = graphics.DrawText(canvas, self.font2, pos, 31, graphics.Color(self.team_colors[hometeam][0][0], self.team_colors[hometeam][0][1], self.team_colors[hometeam][0][2]), homestatline)
+                    if time.time() > timeout:                    
+                        pos -= 1
+                    if (pos + len2 < 0):
+                         pos = canvas.width
+ 
+                    if pos == -(len2-63):              
+                        time.sleep(2)
+                        break
+                    time.sleep(0.04)
+                    canvas = matrix.SwapOnVSync(canvas)
 
             
             
