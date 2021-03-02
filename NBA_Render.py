2d8042880af40493a8ed1c8ff2c8f7ef3e724ff7
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-03-02 15:36:37 -0800

    Update NBA_Render.py

--- NBA_Render.py
+++ NBA_Render.py
@@ -171,7 +171,7 @@ class Render:
 
                 pos = canvas.width
                 while True:         
-                    print(homestatlime)
+                    print(homestatline)
                     for x in range(0, canvas.width):
                         for y in range(20, canvas.height):
                             canvas.SetPixel(x, y, 0, 0, 0)
