2ec69f83d0e46500a3552d18c503b00fb11408ea
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-03-03 01:07:00 -0800

    Update NBA_Render.py

--- NBA_Render.py
+++ NBA_Render.py
@@ -169,23 +169,23 @@ class Render:
 
                 pos = 0
                 while True:         
-                    print(homestatline)
-                    for x in range(0, canvas.width):
-                        for y in range(20, canvas.height):
-                            canvas.SetPixel(x, y, 0, 0, 0)
-                    len1 = graphics.DrawText(canvas, self.font2, pos, 31, graphics.Color(self.team_colors[hometeam][0][0], self.team_colors[hometeam][0][1], self.team_colors[hometeam][0][2]), homestatline)
-                    pos -= 1
-                    if (pos + len1 < 0):
-                        pos = canvas.width
-                    
-                    time.sleep(0.04)
+                #    print(homestatline)
+                 #   for x in range(0, canvas.width):
+                  #      for y in range(20, canvas.height):
+                   #         canvas.SetPixel(x, y, 0, 0, 0)
+                    #len1 = graphics.DrawText(canvas, self.font2, pos, 31, graphics.Color(self.team_colors[hometeam][0][0], self.team_colors[hometeam][0][1], self.team_colors[hometeam][0][2]), homestatline)
+              #      pos -= 1
+               #     if (pos + len1 < 0):
+                #        pos = canvas.width
+                 #   
+                  #  time.sleep(0.04)
                     
             
             canvas = matrix.SwapOnVSync(canvas)
             time.sleep(4)
 
 if __name__=='__main__':
-   # while True:
-    Render().Render_Games()
+    while True:
+        Render().Render_Games()
         
         
