dc90795460935cca88369dca2b2bc24fa736b687
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-03-11 02:19:12 -0800

    Update NBA_Render_Big.py

--- NBA_Render_Big.py
+++ NBA_Render_Big.py
@@ -64,7 +64,7 @@ class Render:
         for game in game_data:
             canvas = matrix.SwapOnVSync(canvas)
             for line in range(0,64):
-                graphics.DrawLine(canvas, 0, line, 128, line, graphics.Color(0, 0, 0))
+                graphics.DrawLine(canvas, 0, line, 128, line, graphics.Color(255, 0, 0))
             image = Image.open("/home/pi/My-NBA-scoreboard/lal.png")
             image1 = Image.open("/home/pi/My-NBA-scoreboard/bkn.png")
                         
