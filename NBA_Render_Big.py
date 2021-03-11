f61b176e23c57809ffaeaf433cf7b6b99acc01d1
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-03-11 01:59:58 -0800

    Update NBA_Render_Big.py

--- NBA_Render_Big.py
+++ NBA_Render_Big.py
@@ -62,8 +62,8 @@ class Render:
             spreads_data = {}
                   
         for game in game_data:
-            for line in range(0,64):
-                graphics.DrawLine(canvas, 0, line, 128, line, graphics.Color(0, 0, 0))
+            #for line in range(0,64):
+             #   graphics.DrawLine(canvas, 0, line, 128, line, graphics.Color(0, 0, 0))
             image = Image.open("/home/pi/My-NBA-scoreboard/lal.png")
             image1 = Image.open("/home/pi/My-NBA-scoreboard/bkn.png")
                         
