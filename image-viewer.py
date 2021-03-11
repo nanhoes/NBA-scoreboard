48e252430ccb364ea33fb3e49c67fa54c8aaa1fc
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-03-10 20:55:01 -0800

    Update image-viewer.py

--- image-viewer.py
+++ image-viewer.py
@@ -18,8 +18,8 @@ options.disable_hardware_pulsing = True
 matrix = RGBMatrix(options = options)
 
 # Make image fit our screen.
-#image.thumbnail((30, matrix.height))
-
+image.thumbnail((30, matrix.height))
+image.offset(5,32)
 matrix.SetImage(image.convert('RGB'))
 
 try:
