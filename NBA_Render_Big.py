ee3d95b55c7e9f0cfed8336cb5e6b35de3c325bc
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-03-15 01:25:03 -0700

    Update NBA_Render_Big.py

--- NBA_Render_Big.py
+++ NBA_Render_Big.py
@@ -30,6 +30,8 @@ class Render:
         self.font.LoadFont("/home/pi/My-NBA-scoreboard/Minimal-Mono-Bold.bdf")
         self.font2 = graphics.Font()
         self.font2.LoadFont("/home/pi/My-NBA-scoreboard/rpi-rgb-led-matrix/fonts/8x13B.bdf")
+        self.font3 = graphics.Font()
+        self.font3.LoadFont("/home/pi/My-NBA-scoreboard/rpi-rgb-led-matrix/fonts/9x15B.bdf")
         self.team_colors = {'ATL': [[225, 58, 62], [100, 100, 100]], 'BOS': [[0, 131, 72], [187, 151, 83]], 'BKN': [[100, 100, 100], [0, 0, 0]], 'CHA': [[29, 17, 96], [0, 140, 168]], 'CHI': [[206, 17, 65], [0, 0, 0]], 'CLE': [[134, 0, 56], [253, 187, 48]], 'DAL': [[0, 125, 197], [196, 206, 211]], 'DEN': [[77, 144, 205], [253, 185, 39]], 'DET': [[237, 23, 76], [0, 107, 182]], 'GSW': [[253, 185, 39], [0, 107, 182]], 'HOU': [[206, 17, 65], [196, 206, 211]], 'LAL': [[253, 185, 39], [85, 37, 130]], 'MEM': [[15, 88, 108], [190, 212, 233]], 'MIA': [[152, 0, 46], [0, 0, 0]], 'MIL': [[0, 71, 27], [240, 235, 210]], 'MIN': [[0, 80, 131], [0, 169, 79]], 'NOP': [[0, 43, 92], [227, 24, 55]], 'NYK': [[0, 107, 182], [245, 132, 38]], 'OKC': [[0, 125, 195], [240, 81, 51]], 'ORL': [[0, 125, 197], [0, 0, 0]], 'PHI': [[237, 23, 76], [0, 107, 182]], 'PHX': [[229, 96, 32], [29, 17, 96]], 'POR': [[224, 58, 62], [186, 195, 201]], 'SAC': [[114, 76, 159], [142, 144, 144]], 'SAS': [[186, 195, 201], [0, 0, 0]], 'TOR': [[206, 17, 65], [0, 0, 0]], 'UTA': [[0, 43, 92], [249, 160, 27]], 'WAS': [[0, 43, 92], [227, 24, 55]], 'IND': [[255, 198, 51], [0, 39, 93]], 'LAC': [[237, 23, 76], [0, 107, 182]]}
             
     def Render_Games(self, printer=False):
@@ -120,21 +122,21 @@ class Render:
                 if timeremaining[0] == 'Q' and (timeremaining[1] >= '4' and (timeremaining[3] == '0' and timeremaining[4] <= '4')): #Q4 or OT < 5min remaining
                     if homescore > awayscore:
                         if (homescore - awayscore) <= 15: #close game
-                            graphics.DrawText(canvas, self.font, 2, 58, graphics.Color(255, 255, 255), game['gameStatusText']) #bright quarter and time remaining
+                            graphics.DrawText(canvas, self.font3, 2, 58, graphics.Color(255, 255, 255), game['gameStatusText']) #bright quarter and time remaining
                             graphics.DrawLine(canvas, 0, 62, 126, 62, graphics.Color(255, 0, 0)) #red line at bottom of screen
                         else:
-                            graphics.DrawText(canvas, self.font, 2, 58, graphics.Color(100, 100, 100), game['gameStatusText'])
+                            graphics.DrawText(canvas, self.font3, 2, 58, graphics.Color(100, 100, 100), game['gameStatusText'])
                     else:
                         if (awayscore - homescore) <= 15: #close game
-                            graphics.DrawText(canvas, self.font, 2, 58, graphics.Color(255, 255, 255), game['gameStatusText']) #bright quarter and time remaining
+                            graphics.DrawText(canvas, self.font3, 2, 58, graphics.Color(255, 255, 255), game['gameStatusText']) #bright quarter and time remaining
                             graphics.DrawLine(canvas, 0, 62, 126, 62, graphics.Color(255, 0, 0)) #red line at bottom of screen
                         else:
-                            graphics.DrawText(canvas, self.font, 2, 58, graphics.Color(100, 100, 100), game['gameStatusText'])
+                            graphics.DrawText(canvas, self.font3, 2, 58, graphics.Color(100, 100, 100), game['gameStatusText'])
                 else: #not a close game or not under 4min
-                    graphics.DrawText(canvas, self.font, 2, 58, graphics.Color(100, 100, 100), game['gameStatusText'])
+                    graphics.DrawText(canvas, self.font3, 2, 58, graphics.Color(100, 100, 100), game['gameStatusText'])
                 
             if game['gameStatus'] == 3: #finished game
-                graphics.DrawText(canvas, self.font, 2, 58, graphics.Color(255, 0, 0), game['gameStatusText'].upper()) #red '"final" text
+                graphics.DrawText(canvas, self.font3, 2, 58, graphics.Color(255, 0, 0), game['gameStatusText'].upper()) #red '"final" text
                 if homescore > awayscore:
                     graphics.DrawText(canvas, self.font, 42, 36, graphics.Color(255, 255, 255), str(homescore)) #bright home winner score
                     graphics.DrawText(canvas, self.font, 42, 16, graphics.Color(100, 100, 100), str(awayscore)) #bright away winner score
@@ -149,9 +151,9 @@ class Render:
                 graphics.DrawText(canvas, self.font2, 41, 14, graphics.Color(100, 100, 100), awayrecord) #away team record
                 graphics.DrawText(canvas, self.font2, 41, 34, graphics.Color(100, 100, 100), homerecord) #home team record
                 if game['gameStatusText'] != 'PPD': #upcoming game
-                    graphics.DrawText(canvas, self.font, 2, 58, graphics.Color(100, 100, 100), game['gameStatusText'][0:len(game['gameStatusText']) - 3])
+                    graphics.DrawText(canvas, self.font3, 2, 58, graphics.Color(100, 100, 100), game['gameStatusText'][0:len(game['gameStatusText']) - 3])
                 if game['gameStatusText'] == 'PPD': #postponed game
-                    graphics.DrawText(canvas, self.font, 2, 58, graphics.Color(100, 100, 100), 'Postponed')
+                    graphics.DrawText(canvas, self.font3, 2, 58, graphics.Color(100, 100, 100), 'Postponed')
                       
             if game['gameStatus'] != 1 and game['period'] !=1:
                 homeleadername = game['gameLeaders']['homeLeaders']['name']
