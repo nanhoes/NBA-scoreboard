072ba4a1515db8171549ab0ca809684205a5a642
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-03-14 18:25:50 -0700

    Update NBA_Render_Big.py

--- NBA_Render_Big.py
+++ NBA_Render_Big.py
@@ -15,7 +15,7 @@ class Render:
         self.options.drop_privileges = False
         self.options.chain_length = 2
         self.options.parallel = 2
-        #self.options.disable_hardware_pulsing = True
+        self.options.disable_hardware_pulsing = True
                    
         now = dt.datetime.now()
         current_time = now.strftime("%H:%M") 
