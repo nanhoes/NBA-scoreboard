11bc617589118b7a984ff583e8faab23ae82f426
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-03-18 15:28:42 -0700

    Update NBA_Render_Big.py

--- NBA_Render_Big.py
+++ NBA_Render_Big.py
@@ -17,13 +17,6 @@ class Render:
         self.options.parallel = 2
         self.options.disable_hardware_pulsing = True
         
-        now = dt.datetime.now()
-        current_time = now.strftime("%H:%M") 
-        if current_time[0] == '0' and (current_time[1] >= '1' and current_time[1] <= '7'):
-            self.options.brightness = 100
-        if current_time[1] > '7' or current_time[0] >= '1':
-            self.options.brightness = 100
-        
         self.path = '/home/pi/My-NBA-scoreboard/'
         
         self.font = graphics.Font()
