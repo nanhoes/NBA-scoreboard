58325024f8f5f6731f2089b29c8d8041dccc4fb8
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-03-17 18:01:31 -0700

    Update NBA_Render_Big.py

--- NBA_Render_Big.py
+++ NBA_Render_Big.py
@@ -135,7 +135,9 @@ class Render:
                         else:
                             graphics.DrawText(canvas, self.font3, 2, 56, graphics.Color(100, 100, 100), game['gameStatusText'])
                 else: #not a close game or not under 4min
-                    graphics.DrawText(canvas, self.font3, 2, 56, graphics.Color(255, 255, 0), game['gameStatusText'])
+                    if game['gameStatusText'][0] == 'Q':
+                        graphics.DrawText(canvas, self.font3, 2, 56, graphics.Color(100, 100, 100), game['gameStatusText'][0:1])
+                        graphics.DrawText(canvas, self.font3, 2, 56, graphics.Color(255, 215, 0), game['gameStatusText'][3:])
                 
             if game['gameStatus'] == 3: #finished game
                 graphics.DrawText(canvas, self.font3, 2, 56, graphics.Color(100, 100, 100), game['gameStatusText'].upper()) #red '"final" text
