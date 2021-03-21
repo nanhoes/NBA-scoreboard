f0c3ae6d109c2421f54eb079214c9f225f0974b4
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-03-21 13:48:39 -0700

    Update NBA_Render_Big.py

--- NBA_Render_Big.py
+++ NBA_Render_Big.py
@@ -230,12 +230,6 @@ class Render:
             
             
 if __name__=='__main__':
-    now = dt.datetime.now()
-    current_time = now.strftime("%H:%M") 
     while True:
-        if current_time[0] == '0' and (current_time[1] >= '1' and current_time[1] <= '7'):
-            os.system('sudo /home/pi/My-NBA-scoreboard/rpi-rgb-led-matrix/examples-api-use/demo -D 7 --led-rows=32 --led-cols=64 --led-parallel=2 --led-pwm-bits=9 --led-slowdown-gpio=2 --led-chain=2 --led-no-hardware-pulse')
-        else:
-            Render().Render_Games()
-      #      Render().Render_Logos()
-
+        Render().Render_Games()
+      
