304266c1347029bc257ef2a3dae236c22aa6f021
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-03-21 20:18:30 -0700

    Update NBA_Render_Big.py

--- NBA_Render_Big.py
+++ NBA_Render_Big.py
@@ -232,7 +232,7 @@ class Render:
                          #   graphics.DrawLine(canvas, 0, line, 128, line, graphics.Color(0, 0, 0))
                 if pos == -100:
                             break
-                canvas = matrix.SwapOnVSync(canvas)            
+                #canvas = matrix.SwapOnVSync(canvas)            
 
                 time.sleep(0.001)
 
