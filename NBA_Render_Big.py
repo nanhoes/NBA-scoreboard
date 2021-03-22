2dc78b591c36ecf59b3b89e21563f4a66b91ff99
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-03-21 17:36:56 -0700

    Update NBA_Render_Big.py

--- NBA_Render_Big.py
+++ NBA_Render_Big.py
@@ -133,7 +133,7 @@ class Render:
             timeremaining = game['gameStatusText']
             if timeremaining[0] == 'Q' and timeremaining[3] == '0':
                 timeremaining = game['gameStatusText'][:3] + game['gameStatusText'][4:]
-            if timeremaining[0] == 'Q' and timeremaining[3] == '0' and timeremaining[4] == '0':
+            if timeremaining[0] == 'Q' and timeremaining[3] == '0' and timeremaining[4] == ':':
                 timeremaining = game['gameStatusText'][:3] + game['gameStatusText'][5:]
             if game['gameStatus'] == 2: #game is live
                 graphics.DrawText(canvas, self.font, 74 - len(str(awayscore))*11, 16, graphics.Color(0, 0, 0), str(awayscore)) 
