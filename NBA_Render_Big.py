778eb6f9cc845d7ee2490c9d8b2970138bf7aacf
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-03-21 18:32:04 -0700

    Update NBA_Render_Big.py

--- NBA_Render_Big.py
+++ NBA_Render_Big.py
@@ -211,7 +211,7 @@ class Render:
                 awayleaderlastname = findawaylastname(awayleadername,2) 
                 awaystatline = awayleadername[0] + '.' + str(awayleaderlastname) + ' ' + str(awayleaderpoints) + '-' + str(awayleaderrebounds) + '-' + str(awayleaderassists)
                 #canvas = matrix.SwapOnVSync(canvas)            
-                graphics.DrawText(canvas, self.font2, 0, 62, graphics.Color(self.team_colors[hometeam][0][0], self.team_colors[hometeam][0][1], self.team_colors[hometeam][0][2]), homestatline)
+                graphics.DrawText(canvas, self.font2, 0, 62, graphics.Color(self.team_colors[hometeam][0][0], self.team_colors[hometeam][0][1], self.team_colors[hometeam][0][2]), homestatline.upper())
         
 
               #  pos = 0
