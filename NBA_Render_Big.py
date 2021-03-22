6901e6904ec46455c25d3d67e4cdffa495fb6244
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-03-21 23:05:53 -0700

    Update NBA_Render_Big.py

--- NBA_Render_Big.py
+++ NBA_Render_Big.py
@@ -226,12 +226,12 @@ class Render:
                 for x in range(0, canvas.width):
                     for y in range(56, canvas.height):
                         canvas.SetPixel(x, y, 255, 255, 255)
-                len1 = graphics.DrawText(canvas, self.font4, 2, 63, graphics.Color(0, 0, 0), awayteam + ': ' + awaystatline.upper())
-                graphics.DrawLine(canvas, 0, 54, 127, pos1, graphics.Color(255, 255, 255))
+                len1 = graphics.DrawText(canvas, self.font4, 2, pos, graphics.Color(0, 0, 0), awayteam + ': ' + awaystatline.upper())
+                #graphics.DrawLine(canvas, 0, 54, 127, pos1, graphics.Color(255, 255, 255))
                 len2 = graphics.DrawText(canvas, self.font4, 2, pos + 10, graphics.Color(0, 0, 0), hometeam + ': ' + homestatline.upper())
-                pos -= 1
+                pos += 1
                 pos1 -= 1 
-                if pos == 63:
+                if pos == 70:
                     time.sleep(2)
                     break
                 if pos1 == 63:
