dec3b4f9963fbed7da8d5c8acfe37e10a278656c
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-03-21 18:39:56 -0700

    Update NBA_Render_Big.py

--- NBA_Render_Big.py
+++ NBA_Render_Big.py
@@ -211,21 +211,20 @@ class Render:
                 awayleaderlastname = findawaylastname(awayleadername,2) 
                 awaystatline = awayleadername[0] + '.' + str(awayleaderlastname) + ' ' + str(awayleaderpoints) + '-' + str(awayleaderrebounds) + '-' + str(awayleaderassists)
                 #canvas = matrix.SwapOnVSync(canvas)            
-                graphics.DrawText(canvas, self.font2, 0, 62, graphics.Color(self.team_colors[hometeam][0][0], self.team_colors[hometeam][0][1], self.team_colors[hometeam][0][2]), homestatline.upper())
+                #graphics.DrawText(canvas, self.font2, 0, 62, graphics.Color(self.team_colors[hometeam][0][0], self.team_colors[hometeam][0][1], self.team_colors[hometeam][0][2]), homestatline.upper())
         
 
-              #  pos = 0
-               # while True:         
-                #    print(homestatline)
-                 #   for x in range(0, canvas.width):
-                  #      for y in range(20, canvas.height):
-                   #         canvas.SetPixel(x, y, 0, 0, 0)
-                    #len1 = graphics.DrawText(canvas, self.font2, pos, 31, graphics.Color(self.team_colors[hometeam][0][0], self.team_colors[hometeam][0][1], self.team_colors[hometeam][0][2]), homestatline)
-              #      pos -= 1
-               #     if (pos + len1 < 0):
-                #        pos = canvas.width
-                 #   
-                  #  time.sleep(0.04)
+                pos = 0
+                while True:         
+                    for x in range(0, canvas.width):
+                        for y in range(25, canvas.height):
+                            canvas.SetPixel(x, y, 0, 0, 0)
+                    len1 = graphics.DrawText(canvas, self.font2, pos, 31, graphics.Color(self.team_colors[hometeam][0][0], self.team_colors[hometeam][0][1], self.team_colors[hometeam][0][2]), homestatline)
+                    pos -= 1
+                    if (pos + len1 < 0):
+                        pos = canvas.width
+                    
+                    time.sleep(0.04)
                     
 
             canvas = matrix.SwapOnVSync(canvas)            
