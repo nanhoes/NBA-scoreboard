8cd69e4910b5e3fb94fe55d3dfba96bf318603d8
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-03-23 16:24:59 -0700

    Update NBA_Render_Big.py

--- NBA_Render_Big.py
+++ NBA_Render_Big.py
@@ -185,11 +185,11 @@ class Render:
                     awayleaderlastname = findawaylastname(awayleadername,2) 
                     awaystatline = awayleadername[0] + '.' + str(awayleaderlastname) + ' ' + str(awayleaderpoints) + '-' + str(awayleaderrebounds) + '-' + str(awayleaderassists)
                     
-                    for line in range(54,64): #statline background
-                        graphics.DrawLine(canvas, 0, line, 127, line, graphics.Color(255, 255, 255))
+                for line in range(54,64): #statline background
+                    graphics.DrawLine(canvas, 0, line, 127, line, graphics.Color(255, 255, 255))
 
-                    len1 = graphics.DrawText(canvas, self.font4, pos, 62, graphics.Color(0, 0, 0), awaystatline.upper() + '  ' + homestatline.upper())
-                    pos -= 1                        
+                len1 = graphics.DrawText(canvas, self.font4, pos, 62, graphics.Color(0, 0, 0), awaystatline.upper() + '  ' + homestatline.upper())
+                pos -= 1                        
                     #if (pos == canvas.width - len1 - 2):
                         #print('Yes')
                         #break
@@ -198,7 +198,7 @@ class Render:
 
                 time.sleep(0.05)
                 canvas = matrix.SwapOnVSync(canvas)
-                break
+                
 
                             
                                       
