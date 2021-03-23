aea1dd095fa1e01ff2f5768a93064325f50d9eed
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-03-23 14:35:16 -0700

    Update NBA_Render_Big.py

--- NBA_Render_Big.py
+++ NBA_Render_Big.py
@@ -30,21 +30,6 @@ class Render:
         self.font4.LoadFont("/home/pi/My-NBA-scoreboard/rpi-rgb-led-matrix/fonts/5x8.bdf")
         self.team_colors = {'ATL': [[225, 58, 62], [255, 255, 255]], 'BOS': [[0, 131, 72], [255, 205, 112]], 'BKN': [[100, 100, 100], [0, 0, 0]], 'CHA': [[29, 17, 96], [0, 140, 168]], 'CHI': [[206, 17, 65], [0, 0, 0]], 'CLE': [[134, 0, 56], [253, 187, 48]], 'DAL': [[0, 125, 197], [196, 206, 211]], 'DEN': [[77, 144, 205], [253, 185, 39]], 'DET': [[237, 23, 76], [0, 107, 182]], 'GSW': [[253, 185, 39], [0, 107, 182]], 'HOU': [[206, 17, 65], [196, 206, 211]], 'LAL': [[253, 185, 39], [85, 37, 130]], 'MEM': [[15, 88, 108], [190, 212, 233]], 'MIA': [[152, 0, 46], [0, 0, 0]], 'MIL': [[0, 71, 27], [240, 235, 210]], 'MIN': [[0, 80, 131], [0, 169, 79]], 'NOP': [[0, 43, 92], [227, 24, 55]], 'NYK': [[0, 107, 182], [245, 132, 38]], 'OKC': [[0, 125, 195], [240, 81, 51]], 'ORL': [[0, 125, 197], [0, 0, 0]], 'PHI': [[237, 23, 76], [0, 107, 182]], 'PHX': [[229, 96, 32], [29, 17, 96]], 'POR': [[224, 58, 62], [186, 195, 201]], 'SAC': [[114, 76, 159], [200, 200, 200]], 'SAS': [[186, 195, 201], [0, 0, 0]], 'TOR': [[206, 17, 65], [0, 0, 0]], 'UTA': [[0, 43, 92], [249, 160, 27]], 'WAS': [[0, 43, 92], [227, 24, 55]], 'IND': [[255, 198, 51], [0, 39, 93]], 'LAC': [[237, 23, 76], [0, 107, 182]]}
                     
-    def Render_Logos(self, printer=False):
-        matrix = RGBMatrix(options=self.options)
-        canvas = matrix.CreateFrameCanvas()
-        image = Image.open("/home/pi/My-NBA-scoreboard/lal.png")
-        image1 = Image.open("/home/pi/My-NBA-scoreboard/bkn.png")
-        
-        # Make image fit our screen.
-        image.thumbnail((30, matrix.height))
-        matrix.SetImage(image.convert('RGB'),5,0)
-        image1.thumbnail((30, matrix.height))
-        matrix.SetImage(image1.convert('RGB'),93,0)
-                    
-        canvas = matrix.SwapOnVSync(canvas)
-
-
     def Render_Games(self, printer=False):
         matrix = RGBMatrix(options=self.options)
         date_range = []
@@ -70,29 +55,8 @@ class Render:
         except:
             print('Error loading spreads data.')
             spreads_data = {}
-            
-        canvas = matrix.CreateFrameCanvas()
-        image = Image.open("/home/pi/My-NBA-scoreboard/lal.png")
-        image1 = Image.open("/home/pi/My-NBA-scoreboard/bkn.png")
-        
-
+       
         for game in game_data:
-
-            pos = 0
-          #  pos1 = 54
-
-            while True:         
-             ####   print('now')
-
-                # Make image fit our screen.
-              #  image.thumbnail((30, matrix.height))
-               # matrix.SetImage(image.convert('RGB'),5,0)
-                #image1.thumbnail((30, matrix.height))
-                #matrix.SetImage(image1.convert('RGB'),93,0)
-
-               # canvas = matrix.SwapOnVSync(canvas)
-
-
                 hometeam = game['homeTeam']['teamTricode']
                 awayteam = game['awayTeam']['teamTricode']
 
@@ -113,24 +77,20 @@ class Render:
                     else:       
                         spread = spreads_data[gamelink]['spread']
                         over_under = spreads_data[gamelink]['over_under']
+                
                 except KeyError:
-                    #print('No spreads for this game.')
                     spread = ''
                     over_under = ''
 
                 for line in range(0,64):	
-                    graphics.DrawLine(canvas, 0, line, 128, line, graphics.Color(0, 0, 0))
-                for x in range(0, canvas.width):
-                    for y in range(56, canvas.height):
-                        canvas.SetPixel(x, y, 255, 255, 255)
-
-
-                for line in range(20,38):
+                    graphics.DrawLine(canvas, 0, line, 128, line, graphics.Color(0, 0, 0)) #clearing matrix
+       
+                for line in range(20,38): #team color square
                     graphics.DrawLine(canvas, 0, line, 38, line, graphics.Color(self.team_colors[hometeam][0][0], self.team_colors[hometeam][0][1], self.team_colors[hometeam][0][2]))
-                for line in range(0,18):
+                for line in range(0,18): #team color square
                     graphics.DrawLine(canvas, 0, line, 38, line, graphics.Color(self.team_colors[awayteam][0][0], self.team_colors[awayteam][0][1], self.team_colors[awayteam][0][2]))
 
-                if game['gameStatus'] != 1:
+                if game['gameStatus'] != 1: #white square for score
                     for line in range(20,38):
                         graphics.DrawLine(canvas, 39, line, 77, line, graphics.Color(255, 255, 255))
                     for line in range(0,18):
@@ -141,7 +101,6 @@ class Render:
                 graphics.DrawText(canvas, self.font, 2, 36, graphics.Color(self.team_colors[hometeam][1][0], self.team_colors[hometeam][1][1], self.team_colors[hometeam][1][2]), hometeam)
                 graphics.DrawText(canvas, self.font, 2, 16, graphics.Color(self.team_colors[awayteam][1][0], self.team_colors[awayteam][1][1], self.team_colors[awayteam][1][2]), awayteam)
 
-
                 homescore = game['homeTeam']['score']
                 awayscore = game['awayTeam']['score']
                 timeremaining = game['gameStatusText']
@@ -149,7 +108,10 @@ class Render:
                     timeremaining = game['gameStatusText'][:3] + game['gameStatusText'][4:]
                 if timeremaining[0] == 'Q' and timeremaining[3] == '0' and timeremaining[4] == ':':
                     timeremaining = game['gameStatusText'][:3] + game['gameStatusText'][5:]
-                if game['gameStatus'] == 2: #game is live
+                    
+                    
+                    
+                if game['gameStatus'] == 2: #GAME IS LIVE
                     graphics.DrawText(canvas, self.font, 74 - len(str(awayscore))*11, 16, graphics.Color(0, 0, 0), str(awayscore)) 
                     graphics.DrawText(canvas, self.font, 74 - len(str(homescore))*11, 36, graphics.Color(0, 0, 0), str(homescore)) 
                     if timeremaining[0] == 'Q' and (timeremaining[1] >= '4' and (len(timeremaining) == 6 or (len(timeremaining) == 7 and timeremaining[3] <= '4'))): #Q4 or OT < 5min remaining
@@ -168,31 +130,23 @@ class Render:
                             else:
                                 graphics.DrawText(canvas, self.font3, 2, 56, graphics.Color(200, 200, 200), timeremaining)
                     else: #not a close game or not under 4min
-                        #if game['gameStatusText'][0:2] == 'Q2':
-                         #   quarter = '2nd'
-                          #  graphics.DrawText(canvas, self.font3, 2, 56, graphics.Color(255, 255, 255), quarter + ' ' + game['gameStatusText'][4:])
                         graphics.DrawText(canvas, self.font3, 2, 56, graphics.Color(186, 212, 191), timeremaining)
 
 
-
-                if game['gameStatus'] == 3: #finished game
+                if game['gameStatus'] == 3: #GAME IS FINISHED
                     graphics.DrawText(canvas, self.font3, 2, 52, graphics.Color(200, 200, 200), game['gameStatusText'].upper()) #56
                     graphics.DrawText(canvas, self.font, 74 - len(str(awayscore))*11, 16, graphics.Color(0, 0, 0), str(awayscore)) 
                     graphics.DrawText(canvas, self.font, 74 - len(str(homescore))*11, 36, graphics.Color(0, 0, 0), str(homescore)) 
 
                     if homescore > awayscore:
-                        #graphics.DrawGlyph(self, canvas, 78, 34, graphics.Color(255, 255, 255), '◄')
-                        #graphics.DrawText(canvas, self.font, 78, 34, graphics.Color(255, 255, 255), '|')
                         for line in range(21,37):
                             graphics.DrawLine(canvas, 80, line, 81, line, graphics.Color(100, 100, 100))
-
                     else:
                         for line in range(1,17):
                             graphics.DrawLine(canvas, 80, line, 81, line, graphics.Color(100, 100, 100))
 
-
-
-                if game['gameStatus'] == 1: #upcoming game
+                            
+                if game['gameStatus'] == 1: #GAME IS UPCOMING
                     awayrecord = str(game['awayTeam']['wins']) + '-' + str(game['awayTeam']['losses'])
                     homerecord = str(game['homeTeam']['wins']) + '-' + str(game['homeTeam']['losses'])
                     graphics.DrawText(canvas, self.font2, 41, 14, graphics.Color(200, 200, 200), awayrecord) #away team record
@@ -202,8 +156,9 @@ class Render:
                     if game['gameStatusText'] == 'PPD': #postponed game
                         graphics.DrawText(canvas, self.font3, 2, 56, graphics.Color(200, 200, 200), 'POSTPONED')
 
+                        
+                        
                 if game['gameStatus'] != 1 and game['period'] !=1:
-                    print('games finished')
                     homeleadername = game['gameLeaders']['homeLeaders']['name']
                     homeleaderpoints = game['gameLeaders']['homeLeaders']['points']
                     homeleaderrebounds = game['gameLeaders']['homeLeaders']['rebounds']
@@ -225,60 +180,11 @@ class Render:
                         return All_words2[n-1]
                     awayleaderlastname = findawaylastname(awayleadername,2) 
                     awaystatline = awayleadername[0] + '.' + str(awayleaderlastname) + ' ' + str(awayleaderpoints) + '-' + str(awayleaderrebounds) + '-' + str(awayleaderassists)
-                    canvas = matrix.SwapOnVSync(canvas)            
-                    #graphics.DrawText(canvas, self.font2, 0, 62, graphics.Color(self.team_colors[hometeam][0][0], self.team_colors[hometeam][0][1], self.team_colors[hometeam][0][2]), homestatline.upper())
-  
-                    #pos = 0
-                 #       while True:         
-                #            graphics.DrawText(canvas, self.font2, 41, 14, graphics.Color(200, 200, 200), 'HELLO') #away team record
-         # ##          len1 = graphics.DrawText(canvas, self.font4, 2, pos, graphics.Color(0, 0, 0), awayteam + ': ' + awaystatline.upper())
-                    #graphics.DrawLine(canvas, 0, 54, 127, pos1, graphics.Color(255, 255, 255))
-          #  ##        len2 = graphics.DrawText(canvas, self.font4, 2, pos + 10, graphics.Color(0, 0, 0), hometeam + ': ' + homestatline.upper())
-           #   ##      pos += 1
-            #    ##    pos1 -= 1 
-             #     ##  if pos == 73:
-              #      ##    time.sleep(2)
-               #       ##  break
-                #  ##  if pos1 == 63:
-                 #   ##    break
-                    print('printing statline')
-                   # len2 = graphics.DrawText(canvas, self.font2, pos, 50, graphics.Color(self.team_colors[awayteam][0][0], self.team_colors[awayteam][0][1], self.team_colors[awayteam][0][2]), awaystatline.upper())
-                    #time.sleep(1)
-                    if (len1 - canvas.width) > 0:
-                        len1 = graphics.DrawText(canvas, self.font4, pos, 63, graphics.Color(0, 0, 0), awayteam + ': ' + awaystatline.upper() + '  ' + hometeam + ': ' + homestatline.upper())
+                                    
 
-                        print('length > width')
-                        if pos == (-1*(len1 - canvas.width) - 2):
-                            print('break')
-                            time.sleep(2)
-                            break
-                                  #  break
-                                #for line in range(58,64):	
-                                 #   graphics.DrawLine(canvas, 0, line, 128, line, graphics.Color(0, 0, 0))
-                        #if pos == -100:
-                         #           break
-                        #time.sleep(0.01)
-
-
-                            #canvas = matrix.SwapOnVSync(canvas)            
-
-                       # time.sleep(0.005)
-                        pos -= 1
-                        print(pos)
-                    else:
-                        time.sleep(3)
-                        break
-                else:
-                    break
-                  ###  canvas = matrix.SwapOnVSync(canvas)            
-
-
+                        
             canvas = matrix.SwapOnVSync(canvas)            
             time.sleep(5)
-        #canvas = matrix.SwapOnVSync(canvas)            
-
-
-
 if __name__=='__main__':
     while True:
         Render().Render_Games()
