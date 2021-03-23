35dc7295a913caba46e20316defe240f5f4b9dcc
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-03-23 16:29:58 -0700

    Update NBA_Render_Big.py

--- NBA_Render_Big.py
+++ NBA_Render_Big.py
@@ -185,24 +185,24 @@ class Render:
                     awayleaderlastname = findawaylastname(awayleadername,2) 
                     awaystatline = awayleadername[0] + '.' + str(awayleaderlastname) + ' ' + str(awayleaderpoints) + '-' + str(awayleaderrebounds) + '-' + str(awayleaderassists)
                     
-                awaystatline = 'Hey '
-                homestatline = ' Hey'
-                for line in range(54,64): #statline background
-                    graphics.DrawLine(canvas, 0, line, 127, line, graphics.Color(255, 255, 255))
+                    awaystatline = 'Hey '
+                    homestatline = ' Hey'
+                    for line in range(54,64): #statline background
+                        graphics.DrawLine(canvas, 0, line, 127, line, graphics.Color(255, 255, 255))
 
-                len1 = graphics.DrawText(canvas, self.font4, pos, 62, graphics.Color(0, 0, 0), awaystatline.upper() + '  ' + homestatline.upper())
-                pos -= 1   
-                print(pos)
-                if pos == -60:
-                    break
-                    #if (pos == canvas.width - len1 - 2):
-                        #print('Yes')
-                        #break
-                    #else:
-                     #   break
+                    len1 = graphics.DrawText(canvas, self.font4, pos, 62, graphics.Color(0, 0, 0), awaystatline.upper() + '  ' + homestatline.upper())
+                    pos -= 1   
+                    print(pos)
+                    if pos == -60:
+                        break
+                        #if (pos == canvas.width - len1 - 2):
+                            #print('Yes')
+                            #break
+                        #else:
+                         #   break
 
-                time.sleep(0.05)
-                canvas = matrix.SwapOnVSync(canvas)
+                    time.sleep(0.05)
+                    canvas = matrix.SwapOnVSync(canvas)
                 
 
                             
