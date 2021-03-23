4d43ae5f1e3a299f5fcbb208e61eed266ab7c517
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-03-23 16:59:55 -0700

    Update NBA_Render_Big.py

--- NBA_Render_Big.py
+++ NBA_Render_Big.py
@@ -192,7 +192,7 @@ class Render:
                     for line in range(56,64): #statline background
                         graphics.DrawLine(canvas, 0, line, 127, line, graphics.Color(255, 255, 255))
 
-                    len1 = graphics.DrawText(canvas, self.font4, pos, 63, graphics.Color(0, 0, 0), awaystatline.upper() + '  ' + homestatline.upper())
+                    len1 = graphics.DrawText(canvas, self.font4, pos, 63, graphics.Color(0, 0, 0), awaystatline.upper())#awaystatline.upper() + '  ' + homestatline.upper())
                     if len1 <= canvas.width:
                         break
                     else:
