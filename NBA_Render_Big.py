310ce3a4d119818cd664eb5ef9f40040667f126f
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-03-23 18:30:57 -0700

    Update NBA_Render_Big.py

--- NBA_Render_Big.py
+++ NBA_Render_Big.py
@@ -194,7 +194,7 @@ class Render:
                     for line in range(56,64): #statline background
                         graphics.DrawLine(canvas, 0, line, 127, line, graphics.Color(255, 255, 255))
                     
-                    len1 = graphics.DrawText(canvas, self.font4, posx, posy, graphics.Color(0, 0, 0), homestatline.upper())#awaystatline.upper() + '  ' + homestatline.upper())
+                    len1 = graphics.DrawText(canvas, self.font4, posx, posy, graphics.Color(0, 0, 0), awaystatline.upper() + '  ' + homestatline.upper())
                     if posy > 61:
                         posy -= 1
                         print(posy)
