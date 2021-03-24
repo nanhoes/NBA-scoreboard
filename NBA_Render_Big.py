96522cb7b49443c9e0942f65269b757ef72953ee
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-03-23 17:53:12 -0700

    Update NBA_Render_Big.py

--- NBA_Render_Big.py
+++ NBA_Render_Big.py
@@ -197,7 +197,7 @@ class Render:
                     len1 = graphics.DrawText(canvas, self.font4, posx, posy, graphics.Color(0, 0, 0), awaystatline.upper() + '  ' + homestatline.upper())
                     posy -= 1
                     if posy == 63:
-                        pass
+                        continue
                     if len1 <= canvas.width:
                         break
                     else:
