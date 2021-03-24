e2b27df812b50466817d295bb05b70bc47a9805d
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-03-23 21:08:21 -0700

    Update NBA_Render.py

--- NBA_Render.py
+++ NBA_Render.py
@@ -85,20 +85,20 @@ class Render:
                 for line in range(0,32):
                     graphics.DrawLine(canvas, 0, line, 64, line, graphics.Color(0, 0, 0))
 
-                for line in range(10,19):
+                for line in range(9,18):
                     graphics.DrawLine(canvas, 0, line, 18, line, graphics.Color(self.team_colors[hometeam][0][0], self.team_colors[hometeam][0][1], self.team_colors[hometeam][0][2]))
                 for line in range(0,9):
                     graphics.DrawLine(canvas, 0, line, 18, line, graphics.Color(self.team_colors[awayteam][0][0], self.team_colors[awayteam][0][1], self.team_colors[awayteam][0][2]))
                 if game['gameStatus'] != 1: #finished game
-                    for line in range(10,19):
+                    for line in range(9,18):
                         graphics.DrawLine(canvas, 19, line, 38, line, graphics.Color(255, 255, 255))
                     for line in range(0,9):
                         graphics.DrawLine(canvas, 19, line, 38, line, graphics.Color(255, 255, 255))
 
 
                 graphics.DrawText(canvas, self.font2, 64 - len(str(over_under))*4, 7, graphics.Color(0, 0, 255), over_under)
-                graphics.DrawText(canvas, self.font2, 64 - len(str(spread))*4, 17, graphics.Color(0, 0, 255), spread)
-                graphics.DrawText(canvas, self.font, 1, 18, graphics.Color(self.team_colors[hometeam][1][0], self.team_colors[hometeam][1][1], self.team_colors[hometeam][1][2]), hometeam)
+                graphics.DrawText(canvas, self.font2, 64 - len(str(spread))*4, 16, graphics.Color(0, 0, 255), spread)
+                graphics.DrawText(canvas, self.font, 1, 17, graphics.Color(self.team_colors[hometeam][1][0], self.team_colors[hometeam][1][1], self.team_colors[hometeam][1][2]), hometeam)
                 graphics.DrawText(canvas, self.font, 1, 8, graphics.Color(self.team_colors[awayteam][1][0], self.team_colors[awayteam][1][1], self.team_colors[awayteam][1][2]), awayteam)
 
 
