626a4878f733c7a13d78a725f22f5fa189b71770
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-03-23 20:40:47 -0700

    Update NBA_Render.py

--- NBA_Render.py
+++ NBA_Render.py
@@ -133,10 +133,10 @@ class Render:
                 graphics.DrawText(canvas, self.font, 35 - len(str(homescore))*5, 18, graphics.Color(0, 0, 0), str(homescore))
 
                 if homescore > awayscore:
-                    for line in range(11,18):
+                    for line in range(13,16):
                         graphics.DrawLine(canvas, 40, line, 41, line, graphics.Color(100, 100, 100))
                 else:
-                    for line in range(1,8):
+                    for line in range(3,6):
                         graphics.DrawLine(canvas, 40, line, 41, line, graphics.Color(100, 100, 100))
 
 
