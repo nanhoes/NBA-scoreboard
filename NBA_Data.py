d2a0d57570a196985e98d963c282fcde33b1c826
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-04-04 17:05:52 -0700

    Update NBA_Data.py

--- NBA_Data.py
+++ NBA_Data.py
@@ -59,5 +59,5 @@ if __name__ == '__main__':
         #print('Successfully gathered data.')
         #except:
         #print('Error requesting data - retrying in 120 seconds')
-        time.sleep(1)
+        time.sleep(120)
         #test = False
