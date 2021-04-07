f6a8585668dab4740710b5c88e56abb2d8b93ae5
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-04-07 15:12:29 -0700

    Update NBA_Render.py

--- NBA_Render.py
+++ NBA_Render.py
@@ -101,17 +101,17 @@ class Render:
             graphics.DrawText(canvas, self.font, 1, 18, graphics.Color(self.team_colors[hometeam][1][0], self.team_colors[hometeam][1][1], self.team_colors[hometeam][1][2]), hometeam)
             graphics.DrawText(canvas, self.font, 1, 8, graphics.Color(self.team_colors[awayteam][1][0], self.team_colors[awayteam][1][1], self.team_colors[awayteam][1][2]), awayteam)
 
-            
-            for line in range(21,31):
-                graphics.DrawLine(canvas, 59, line, 60, line, graphics.Color(0, 0, 255))   
-                graphics.DrawLine(canvas, 62, line, 62, line, graphics.Color(255, 0, 0))
-                graphics.DrawLine(canvas, 61, line, 61, line, graphics.Color(255, 255, 255))   
-            for line in range(23,27):
-                graphics.DrawLine(canvas, 59, line, 59, line, graphics.Color(255, 255, 255))   
-            for line in range(22,28):
+            # NBA Logo
+            for line in range(20,30):
+                graphics.DrawLine(canvas, 58, line, 59, line, graphics.Color(0, 0, 255))   
+                graphics.DrawLine(canvas, 61, line, 61, line, graphics.Color(255, 0, 0))
                 graphics.DrawLine(canvas, 60, line, 60, line, graphics.Color(255, 255, 255))   
-            for line in range(25,27):
-                graphics.DrawLine(canvas, 62, line, 62, line, graphics.Color(255, 255, 255))   
+            for line in range(22,26):
+                graphics.DrawLine(canvas, 58, line, 58, line, graphics.Color(255, 255, 255))   
+            for line in range(21,27):
+                graphics.DrawLine(canvas, 59, line, 59, line, graphics.Color(255, 255, 255))   
+            for line in range(24,26):
+                graphics.DrawLine(canvas, 61, line, 61, line, graphics.Color(255, 255, 255))   
 
 
 
