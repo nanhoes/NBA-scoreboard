5e90fa27a3414a6c36996c05a1f361240da1377f
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-04-07 17:07:42 -0700

    Update NBA_Render.py

--- NBA_Render.py
+++ NBA_Render.py
@@ -136,6 +136,8 @@ class Render:
                     timeremaining = '3RD OT' + timeremaining[2:]
                 if timeremaining[1] == '8':
                     timeremaining = '4TH OT' + timeremaining[2:]
+            if timeremaining == 'Half':
+                timeremaining = 'HALFTIME'
                            
             if game['gameStatus'] == 2: #game is live
                 graphics.DrawText(canvas, self.font, 35 - len(str(awayscore))*5, 8, graphics.Color(0, 0, 0), str(awayscore)) 
