95ee1cd2243f36fe1e78f08d39899936b5ff2900
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-04-07 18:40:44 -0700

    Update NBA_Render.py

--- NBA_Render.py
+++ NBA_Render.py
@@ -146,7 +146,7 @@ class Render:
                 graphics.DrawText(canvas, self.font, 35 - len(str(homescore))*5, 18, graphics.Color(0, 0, 0), str(homescore))
                 print(game['gameStatusText'])
                 print(timeremaining)
-                if timeremaining[0] >= '4' and (timeremaining[4] <= '4' or timeremaining[4] == ':'): #Q4 or OT < 5min remaining
+                if (game['gameStatusText'][0] == 'Q' and game['gameStatusText'][1] >= '4') and game['gameStatusText'][4] <= '4': #Q4 or OT < 5min remaining
                     if homescore > awayscore:
                         if (homescore - awayscore) <= 15: #close game
                             graphics.DrawText(canvas, self.font3, 2, 28, graphics.Color(255, 255, 255), timeremaining) #bright quarter and time remaining
