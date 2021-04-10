8941e9ff1e83d1b51071278530b2a7822dafdf41
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-04-09 18:25:09 -0700

    Update NBA_Render_Big.py

--- NBA_Render_Big.py
+++ NBA_Render_Big.py
@@ -108,8 +108,8 @@ class Render:
 
           # NBA Logo
                 for line in range(40,54):
-                    graphics.DrawLine(canvas, 119, line, 121, line, graphics.Color(0, 0, 255))   
-                    graphics.DrawLine(canvas, 124, line, 125, line, graphics.Color(255, 0, 0))
+                    graphics.DrawLine(canvas, 119, line, 122, line, graphics.Color(0, 0, 255))   
+                    graphics.DrawLine(canvas, 123, line, 124, line, graphics.Color(255, 0, 0))
                 for line in range(43,46):
                     graphics.DrawLine(canvas, 119, line, 119, line, graphics.Color(255, 255, 255))   
                 for line in range(42,47):
