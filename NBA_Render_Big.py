f8883189b5f29f3b37e0cb75894594eb3ebd14b8
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-04-09 17:19:49 -0700

    Update NBA_Render_Big.py

--- NBA_Render_Big.py
+++ NBA_Render_Big.py
@@ -105,6 +105,27 @@ class Render:
                 graphics.DrawText(canvas, self.font, 2, 36, graphics.Color(self.team_colors[hometeam][1][0], self.team_colors[hometeam][1][1], self.team_colors[hometeam][1][2]), hometeam)
                 graphics.DrawText(canvas, self.font, 2, 16, graphics.Color(self.team_colors[awayteam][1][0], self.team_colors[awayteam][1][1], self.team_colors[awayteam][1][2]), awayteam)
 
+                
+                      # NBA Logo
+            for line in range(20,30):
+                graphics.DrawLine(canvas, 58, line, 60, line, graphics.Color(0, 0, 255))   
+                graphics.DrawLine(canvas, 60, line, 61, line, graphics.Color(255, 0, 0))
+                graphics.DrawLine(canvas, 60, line, 60, line, graphics.Color(255, 255, 255))   
+            for line in range(22,26):
+                graphics.DrawLine(canvas, 58, line, 58, line, graphics.Color(255, 255, 255))   
+            for line in range(21,27):
+                graphics.DrawLine(canvas, 59, line, 59, line, graphics.Color(255, 255, 255))   
+            for line in range(24,26):
+                graphics.DrawLine(canvas, 61, line, 61, line, graphics.Color(255, 255, 255))   
+                
+                
+                
+                
+                
+                
+                
+                
+                
                 homescore = game['homeTeam']['score']
                 awayscore = game['awayTeam']['score']
                 timeremaining = game['gameStatusText']
