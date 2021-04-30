5a5450b64b60cf5bd86b0067f4e69d45eb000c24
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-04-30 16:48:35 -0700

    Update NBA_Spreads.py

--- NBA_Spreads.py
+++ NBA_Spreads.py
@@ -7,69 +7,77 @@ class NBA_Spreads:
     def __init__(self):
         self.path = '/home/pi/My-NBA-scoreboard/NBASpreads.json'
         self.path_live = '/home/pi/My-NBA-scoreboard/NBASpreadsLive.json'
-        
+
     def Spreads_New_Day(self):
         url = 'https://www.bovada.lv/services/sports/event/v2/events/A/description/basketball/nba'
 
         response = requests.get(url).json()
         data = {}
         for game in response[0]['events']:
-            
+
             if game['type'] != 'GAMEEVENT' or game['live'] == True:
                 #print('Not a game.')
                 continue
-            
+
             gamelink = game['link'][0:len(game['link'])-4]
             if dt.datetime.strftime(dt.datetime.now(), '%Y%m%d') != gamelink[len(gamelink)-8:len(gamelink)]:
                 #print(gamelink[len(gamelink)-9:-1])
                 continue
-            data[gamelink] = {}
-            data[gamelink]['hometeam'] = game['displayGroups'][0]['markets'][1]['outcomes'][1]['description']
-            data[gamelink]['awayteam'] = game['displayGroups'][0]['markets'][1]['outcomes'][0]['description']
-            data[gamelink]['spread'] = game['displayGroups'][0]['markets'][1]['outcomes'][1]['price']['handicap']
-            data[gamelink]['over_under'] = game['displayGroups'][0]['markets'][2]['outcomes'][0]['price']['handicap']
-            
+            try:
+                data[gamelink] = {}
+                data[gamelink]['hometeam'] = game['displayGroups'][0]['markets'][1]['outcomes'][1]['description']
+                data[gamelink]['awayteam'] = game['displayGroups'][0]['markets'][1]['outcomes'][0]['description']
+                data[gamelink]['spread'] = game['displayGroups'][0]['markets'][1]['outcomes'][1]['price']['handicap']
+                data[gamelink]['over_under'] = game['displayGroups'][0]['markets'][2]['outcomes'][0]['price']['handicap']
+            except:
+                print('Error gathering live data')
         with open(self.path, 'w') as file:
             json.dump(data, file)
-        
-        
+
+
         return None
-            
+
     def Spreads_Update(self):
         url = 'https://www.bovada.lv/services/sports/event/v2/events/A/description/basketball/nba'
 
         response = requests.get(url).json()
-        
+
         with open(self.path, 'r') as file:
             data = json.load(file)
-        
+
         with open(self.path_live, 'r') as file:
             data_live = json.load(file)
-            
+
         for game in response[0]['events']:
-            
+
             if game['type'] != 'GAMEEVENT' or game['live'] == True:
                 continue
-            
+
             gamelink = game['link'][0:len(game['link'])-4]
             if dt.datetime.strftime(dt.datetime.now(), '%Y%m%d') != gamelink[len(gamelink)-8:len(gamelink)]:
                 continue
-            data[gamelink] = {}
-            data[gamelink]['hometeam'] = game['displayGroups'][0]['markets'][1]['outcomes'][1]['description']
-            data[gamelink]['awayteam'] = game['displayGroups'][0]['markets'][1]['outcomes'][0]['description']
-            data[gamelink]['spread'] = game['displayGroups'][0]['markets'][1]['outcomes'][1]['price']['handicap']
-            data[gamelink]['over_under'] = game['displayGroups'][0]['markets'][2]['outcomes'][0]['price']['handicap']
-        
+            try:
+                data[gamelink] = {}
+                data[gamelink]['hometeam'] = game['displayGroups'][0]['markets'][1]['outcomes'][1]['description']
+                data[gamelink]['awayteam'] = game['displayGroups'][0]['markets'][1]['outcomes'][0]['description']
+                data[gamelink]['over_under'] = game['displayGroups'][0]['markets'][2]['outcomes'][0]['price']['handicap']
+            except:
+                print('Error gathering live data')
+
         with open(self.path, 'w') as file:
             json.dump(data, file)
-        
+
         data_live = {}
         for game in response[0]['events']:
-            
+
             if game['type'] != 'GAMEEVENT' or game['live'] != True:
                 #print('Not a game.')
+
+                #print(game['type'])
+                #print(game['live'])
+
                 continue
-            
+
             gamelink = game['link'][0:len(game['link'])-4]
             if dt.datetime.strftime(dt.datetime.now(), '%Y%m%d') != gamelink[len(gamelink)-8:len(gamelink)]:
                 #print(gamelink[len(gamelink)-9:-1])
@@ -81,16 +89,18 @@ class NBA_Spreads:
                 data_live[gamelink]['spread'] = game['displayGroups'][0]['markets'][1]['outcomes'][1]['price']['handicap']
                 data_live[gamelink]['over_under'] = game['displayGroups'][0]['markets'][2]['outcomes'][0]['price']['handicap']
             except:
-                print('Error gatehring live data')
+                print('Error gathering live data')
         with open(self.path_live, 'w') as file:
             json.dump(data_live, file)
-        
+
         return None
 
 if __name__ == '__main__':
     #NBA_Spreads().Spreads_Update()
     NBA_Spreads().Spreads_New_Day()
-    
-    
+
+
 #Set up a script similar to the NBA data that updates spreads every 2 minutes for live data only
 #Sync spreads update and new day to both start at noon, offset the updates by one minute
+
+
