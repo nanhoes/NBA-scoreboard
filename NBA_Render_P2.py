92aa47cfc74ca691d365b5dc858a387a3fd05d78
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-07-02 14:51:41 -0700

    Update NBA_Render_P2.py

--- NBA_Render_P2.py
+++ NBA_Render_P2.py
@@ -60,7 +60,8 @@ class Render:
         canvas = matrix.CreateFrameCanvas()
         if game_data == []:
             print('no games')
-            graphics.DrawText(canvas, self.font, 14, 39, graphics.Color(255,0,0), 'NO GAMES\nTODAY')
+            graphics.DrawText(canvas, self.font, 14, 32, graphics.Color(200,200,200), 'NO GAMES')
+            graphics.DrawText(canvas, self.font, 20, 46, graphics.Color(200,200,200), 'TODAY')
             canvas = matrix.SwapOnVSync(canvas)
             signal.pause()
 
