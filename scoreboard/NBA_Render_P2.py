f0fefb4b3836fe32117fd4bb8d2834bbd24c7cf3
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-08-05 13:16:54 -0700

    Update NBA_Render_P2.py

--- scoreboard/NBA_Render_P2.py
+++ scoreboard/NBA_Render_P2.py
@@ -21,15 +21,14 @@ class Render:
         config.read(filename)
         
         self.options = RGBMatrixOptions()
-        self.options.gpio_slowdown = int(config['DEFAULT']['gpio_slowdown'])
-        self.options.rows = int(config['DEFAULT']['rows'])
-        self.options.cols = int(config['DEFAULT']['columns'])
-        self.options.drop_privileges = config['DEFAULT']['drop_privileges']
-        self.options.hardware_mapping = config['DEFAULT']['hardware_mapping']
-        self.options.row_address_type = int(config['DEFAULT']['row_address_type']
+        self.options.gpio_slowdown = 1
+        self.options.rows = 64
+        self.options.cols = 128
+        self.options.drop_privileges = 'False'
+        self.options.hardware_mapping = 'adafruit-hat-pwm'
+        self.options.row_address_type = 3
         #self.options.panel_type = 'FM6126A'
-        self.options.brightness = int(config['DEFAULT']['brightness'])
-        
+         
         self.path = '/home/pi/NBA-scoreboard/scoreboard/'
         
         self.font = graphics.Font()
