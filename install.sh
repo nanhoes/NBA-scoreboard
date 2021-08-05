6cc634674149f2daf5d284837001f09353993725
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-08-05 00:21:40 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "Installing rpi-rgb-led-matrix:"
 git clone https://github.com/hzeller/rpi-rgb-led-matrix.git
 
 cd
-cd /home/pi/My-NBA-scoreboard/scoreboard/rpi-rgb-led-matrix
+cd /home/pi/My-NBA-scoreboard/rpi-rgb-led-matrix
 
 echo "Installing python3:"
 sudo apt-get update && sudo apt-get install python3-dev python3-pillow -y
@@ -17,7 +17,7 @@ cd
 sudo apt-get install otf2bdf
 cd /home/pi/My-NBA-scoreboard
 otf2bdf -v -o Minimal-Mono-Bold.bdf -r 72 -p 18 /home/pi/My-NBA-scoreboard/Minimal-Mono-Bold.otf
-cd /home/pi/My-NBA-scoreboard/scoreboard
+cd /home/pi/My-NBA-scoreboard
 
 install_path=$(pwd)
 echo "Installing flask library:"
