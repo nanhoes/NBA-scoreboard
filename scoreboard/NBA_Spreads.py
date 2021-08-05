82f50d0f9014db397061fbee7082427b673dcccc
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-08-05 00:45:06 -0700

    Update NBA_Spreads.py

--- scoreboard/NBA_Spreads.py
+++ scoreboard/NBA_Spreads.py
@@ -5,8 +5,8 @@ import datetime as dt
 
 class NBA_Spreads:
     def __init__(self):
-        self.path = '/home/pi/My-NBA-scoreboard/scoreboard/NBASpreads.json'
-        self.path_live = '/home/pi/My-NBA-scoreboard/scoreboard/NBASpreadsLive.json'
+        self.path = '/home/pi/NBA-scoreboard/scoreboard/NBASpreads.json'
+        self.path_live = '/home/pi/NBA-scoreboard/scoreboard/NBASpreadsLive.json'
 
     def New_Day(self):
         url = 'https://www.bovada.lv/services/sports/event/v2/events/A/description/basketball/nba'
