a39a0d6cc68d833821aba678d82cf9fbb021dbca
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-08-04 21:46:23 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,46 @@
+#!/bin/bash
+
+echo "Installing rpi-rgb-led-matrix:"
 git clone https://github.com/hzeller/rpi-rgb-led-matrix.git
 
 cd
 cd /home/pi/My-NBA-scoreboard/rpi-rgb-led-matrix
 
+echo "Installing python3:"
 sudo apt-get update && sudo apt-get install python3-dev python3-pillow -y
 make build-python PYTHON=$(which python3)
 sudo make install-python PYTHON=$(which python3)
 sudo apt-get install python3-bs4
 
+echo "Installing font dependencies:"
 cd
 sudo apt-get install otf2bdf
 cd /home/pi/My-NBA-scoreboard
 otf2bdf -v -o Minimal-Mono-Bold.bdf -r 72 -p 18 /home/pi/My-NBA-scoreboard/Minimal-Mono-Bold.otf
-sudo python3 /home/pi/My-NBA-scoreboard/NBA_Data.py
-sudo python3 /home/pi/My-NBA-scoreboard/Spreads_New_Day.py
-sudo python3 /home/pi/My-NBA-scoreboard/NBA_Spreads.py
-sudo python3 /home/pi/My-NBA-scoreboard/NBA_Render.py
+
+install_path=$(pwd)
+echo "Installing flask library:"
+pip install flask --upgrade
+echo "Creating render service:"
+sudo cp ./config/render.service /etc/systemd/system/
+sudo sed -i -e "/\[Service\]/a ExecStart=python3 ${install_path}/NBA_Render_P2.py < /dev/zero &> /dev/null &" /etc/systemd/system/render.service
+sudo mkdir /etc/systemd/system/render.service.d
+render_env_path=/etc/systemd/system/render.service.d/render_env.conf
+sudo touch $render_env_path
+sudo echo "[Service]" >> $render_env_path
+sudo systemctl daemon-reload
+sudo systemctl start render
+sudo systemctl enable render
+echo "...done"
+
+echo -n "In order to finish setup a reboot is necessary..."
+echo -n "REBOOT NOW? [y/N] "
+read
+if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then
+        echo "Exiting without reboot."
+        exit 0
+fi
+echo "Reboot started... Will start scoreboard on reboot..."
+reboot
+sleep infinity
+
