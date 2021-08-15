2b6ce586ef39ffb93471f7d6154ddc2729cef99e
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-08-14 21:12:15 -0700

    Update gif_start.sh

--- gif_start.sh
+++ gif_start.sh
@@ -3,4 +3,4 @@ install_path=/home/pi/NBA-scoreboard
 source ${install_path}/config-parser/config-parser.sh
 config_parser ${install_path}/config/matrix_options.ini
 config.section.DEFAULT
-sudo ${install_path}/rpi-rgb-led-matrix/utils/led-image-viewer --led-rows=${rows} --led-cols=${columns} --led-chain=${chain_length} --led-parallel=${parallel} --led-gpio-mapping=${hardware_mapping} —led-slowdown-gpio=${slowdown_gpio} --led-brightness=${brightness} --led-row-addr-type=${row_address_type} images/${gif}
+sudo ${install_path}/rpi-rgb-led-matrix/utils/led-image-viewer --led-rows=${rows} --led-cols=${columns} --led-chain=${chain_length} --led-parallel=${parallel} --led-gpio-mapping=${hardware_mapping} —led-slowdown-gpio=${slowdown_gpio} --led-brightness=${brightness} --led-row-addr-type=${row_address_type} images/${gif}.gif
