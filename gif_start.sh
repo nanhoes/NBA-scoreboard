6c3da75f5a2b71c1e517c667f4c89ddd7a12887d
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-08-15 22:48:20 -0700

    Update and rename gifviewer_start.sh to gif_start.sh

--- gif_start.sh
+++ gif_start.sh
@@ -3,4 +3,4 @@ install_path=/home/pi/NBA-scoreboard
 source ${install_path}/config-parser/config-parser.sh
 config_parser ${install_path}/config/matrix_options.ini
 config.section.DEFAULT
-sudo ${install_path}/rpi-rgb-led-matrix/utils/led-image-viewer --led-gpio-mapping=${hardware_mapping} --led-cols=${columns} ${install_path}/images/${gifviewer}.gif
+sudo ${install_path}/rpi-rgb-led-matrix/utils/led-image-viewer --led-gpio-mapping=${hardware_mapping} --led-cols=${columns} ${install_path}/images/${gif}.gif
