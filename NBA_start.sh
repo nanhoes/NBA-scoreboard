953165419bce0fedb90627d91b03da40222aa0c2
Juliano <nanhoes@icloud.com>
2021-08-28 13:44:10 -0700

    removed service start scripts from main folder

--- NBA_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-install_path=/home/pi/NBA-scoreboard
-source ${install_path}/config-parser/config-parser.sh
-config_parser ${install_path}/config/matrix_options.ini
-config.section.DEFAULT
-sudo python3 ${install_path}/scoreboard/${render_file}
--- gif_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-install_path=/home/pi/NBA-scoreboard
-source ${install_path}/config-parser/config-parser.sh
-config_parser ${install_path}/config/matrix_options.ini
-config.section.DEFAULT
-sudo ${install_path}/rpi-rgb-led-matrix/utils/led-image-viewer --led-gpio-mapping=${hardware_mapping} --led-cols=${columns} --led-chain=${chain_length} --led-parallel=${parallel} --led-gpio-mapping=${hardware_mapping} --led-slowdown-gpio=${gpio_slowdown} --led-brightness=${brightness} --led-row-addr-type=${row_address_type} ${install_path}/board_images/${gif}
