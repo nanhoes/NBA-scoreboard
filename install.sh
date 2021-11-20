7c4fbe3d990d57711763564ed33313d980932d13
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-11-19 19:34:10 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -228,16 +228,6 @@ echo "...done"
 read -n 1 -r -s -p $'\n----------------------------------\nPlease paste crontab from github now... PRESS ANY BUTTON TO CONTINUE...\n'
 sudo crontab -e
 
-echo "Installing wifi-connect"
+echo "Installing wifi-connect... Will reboot after install..."
 cd NBA-scoreboard && chmod +x service_scripts/start-wifi-connect.sh && chmod +x service_scripts/install-wifi-connect.sh && nohup bash service_scripts/install-wifi-connect.sh & tail -f nohup.out
 
-echo -n "In order to finish setup a reboot is necessary..."
-echo -n "REBOOT NOW? [y/N] "
-read
-if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then
-        echo "Exiting without reboot."
-        exit 0
-fi
-echo "Reboot started..."
-reboot
-sleep infinity
