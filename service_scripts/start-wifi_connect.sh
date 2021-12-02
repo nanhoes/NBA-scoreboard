324e2faa4c19d54271e7a084c66064da903256e3
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-12-01 18:11:32 -0800

    Update start-wifi_connect.sh

--- service_scripts/start-wifi_connect.sh
+++ service_scripts/start-wifi_connect.sh
@@ -28,7 +28,8 @@ if [ $? -eq 0 ]; then
 else
     printf 'Starting WiFi Connect\n'
     sudo systemctl stop client
-    sudo python3 /home/pi/NBA-scoreboard/wifi_connecting/Wifi_Not_Connected.py && sudo wifi-connect -s NBA-WiFi-Setup
+    sudo python3 /home/pi/NBA-scoreboard/wifi_connecting/Wifi_Not_Connected.py & 
+    sudo wifi-connect -s NBA-WiFi-Setup -a 10
     sudo pkill -f /home/pi/NBA-scoreboard/wifi_connecting/Wifi_Not_Connected.py
     sudo python3 /home/pi/NBA-scoreboard/wifi_connecting/Wifi_Connected.py
 
