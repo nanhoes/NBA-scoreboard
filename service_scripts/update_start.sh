9f270db26cb43ca3d9683c9e7ba4d5be6fad974b
nanhoes <68497310+nanhoes@users.noreply.github.com>
2021-12-16 02:02:28 -0800

    Update update_start.sh

--- service_scripts/update_start.sh
+++ service_scripts/update_start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+install_path=/home/pi/NBA-scoreboard
+source ${install_path}/config-parser/config-parser.sh
+config_parser ${install_path}/config/matrix_options.ini
+config.section.DEFAULT
+sudo ${install_path}/rpi-rgb-led-matrix/utils/led-image-viewer --led-rows=${rows} --led-cols=${columns} --led-chain=${chain_length} --led-parallel=${parallel} --led-gpio-mapping=${hardware_mapping} --led-slowdown-gpio=${gpio_slowdown} --led-brightness=${brightness} --led-row-addr-type=${row_address_type} -C ${install_path}/board_images/loading.gif
 cd /home/pi/NBA-scoreboard
 git pull
 sudo python3 /home/pi/NBA-scoreboard/update_handling/no_update.py
 sudo systemctl daemon-reload
+sleep 5
 sudo reboot
