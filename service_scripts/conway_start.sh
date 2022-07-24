c57de8e51e2ee1d2bb6fc5465d89ae2bffcea8f0
nanhoes <nanhoes@icloud.com>
2022-07-23 19:58:10 -0700

    lengthened conway animation

--- service_scripts/conway_start.sh
+++ service_scripts/conway_start.sh
@@ -3,4 +3,4 @@ install_path=/home/pi/NBA-scoreboard
 source ${install_path}/config-parser/config-parser.sh
 config_parser ${install_path}/config/matrix_options.ini
 config.section.DEFAULT
-sudo ${install_path}/rpi-rgb-led-matrix/examples-api-use/demo --led-rows=${rows} --led-cols=${columns} --led-chain=${chain_length} --led-parallel=${parallel} --led-gpio-mapping=${hardware_mapping} --led-slowdown-gpio=${gpio_slowdown} --led-brightness=${brightness} --led-row-addr-type=${row_address_type} --led-pwm-bits=10 -D 7
+sudo ${install_path}/rpi-rgb-led-matrix/examples-api-use/demo --led-rows=${rows} --led-cols=${columns} --led-chain=${chain_length} --led-parallel=${parallel} --led-gpio-mapping=${hardware_mapping} --led-slowdown-gpio=${gpio_slowdown} --led-brightness=${brightness} --led-row-addr-type=${row_address_type} --led-pwm-bits=10 -D 7 -m 75
